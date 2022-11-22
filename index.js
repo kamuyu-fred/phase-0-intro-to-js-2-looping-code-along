bbe3774c0fa65cde9bfec870d31aab4efa72cf8c
fred <daktarispacetime@gmail.com>
2022-11-22 05:45:03 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
 // Code your solutions in this file
+function writeCards(name, event){
+    const gifts = []
+    for(let i = 0; i < name.length; i++){
+        gifts.push(`Thank you, ${name[i]}, for the wonderful surprise gift!`)
+    }
+    return gifts
+}
+function countDown(i){
+    for( i = 10; i >= 0; i--){
+        console.log(i)
+    }
+}
